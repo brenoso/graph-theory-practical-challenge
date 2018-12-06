63d80aaa83825d9338ab977b74c95268342d1b1f
Guilherme Haddad <guilherme.haddad@hitransportes.com.br>
2018-12-06 13:31:40 -0200

    Refatoração e organização do repositório com todo material gerado para resolução do problema.

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.reader import Reader
+
+reader = Reader()
+
+qtd_clientes = reader.get_qtd_clientes()
+qtd_sub_regioes = reader.get_qtd_sub_regioes()
+qtd_tipos_veiculos = reader.get_qtd_tipos_veiculos()
+horas_jornada_diaria = reader.get_horas_jornada_diaria()
+coordenadas_sub_regioes = reader.get_coordenadas_sub_regioes()
--- src/reader.py
+++ src/reader.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-class Arquivo(object):
+class Reader(object):
 
     '''
     Construtor da classe
