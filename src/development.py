676abee21b8256a3d9fd0e221ffc7fe9897b0573
Breno Oliveira <brn753@gmail.com>
2018-12-11 21:40:24 -0200

    refatorando para o jupyter

--- src/development.py
+++ src/development.py
@@ -1,19 +1,33 @@
 
 # coding: utf-8
 
-# ## Fase 0 - Leitura do arquivo de entrada, sumarização e visualização dos dados
+# # GCC218 - Algoritmos em Grafos
+# ## Desafio - ShopMais
+# 
+# `Breno Oliveira - 2015`
+# 
+# `Guilherme Baviera - 20`
+# 
+# `Guilherme Henrique Haddad - 201520615`
+# 
+# 
+# 
+# ## Fase 0
+# 
+# #### 1 - Leitura do arquivo de entrada
+# #### 2 - Sumarização
+# #### 3 - Visualização dos dados
 
 # In[1]:
 
+
 with open('C:/Users/Breno/Desktop/graph-theory-practical-challenge/assets/InstanciaTeste.txt') as file:
-# with open('C:/Users/Guilherme/Desktop/graph-theory-practical-challenge/assets/InstanciaTeste.txt') as file:
-# with open('/home/breno/projetos/graph-theory-practical-challenge/assets/InstanciaTeste.txt') as file:
     N = int(file.readline())
     R = int(file.readline())
     K = int(file.readline())
     H = int(file.readline())
-    distribuitions = [file.readline().split() for i in range(R)]
-    points = ["".join([file.readline(), "-1"]).split() for i in range(N-R)]
+    centros_distribuicao = [file.readline().split() for i in range(R)]
+    clientes = ["".join([file.readline(), "-1"]).split() for i in range(N-R)]
     vehicles = [file.readline().split(' ') for i in range(K)]
     
 
@@ -24,18 +38,16 @@ with open('C:/Users/Breno/Desktop/graph-theory-practical-challenge/assets/Instan
 # 
 # Isto ajuda a elucidar a distribuição dos clientes para esse problema.
 
-# In[3]:
+# In[8]:
 
 
 from matplotlib import pyplot as plt
 import random
 #para todos os centros:
-plt.scatter([float(x) for x,_,_,_,_ in distribuitions], [float(y) for _,y,_,_,_ in distribuitions], color='red')
+plt.scatter([float(x) for x,_,_,_,_ in centros_distribuicao], [float(y) for _,y,_,_,_ in centros_distribuicao], color='red')
 #para todos os clientes
-plt.scatter([float(x) for x,_,_,_,_,_ in points], [float(y) for _,y,_,_,_,_ in points], color='blue')
+plt.scatter([float(x) for x,_,_,_,_,_ in clientes], [float(y) for _,y,_,_,_,_ in clientes], color='blue')
 
-#for p,d in zip(points, distribuitions):
-#plt.annotate('dist', xy=(d[0],d[1]), xytext=(5,-5), textcoords='offset points')
 
 #axis = Define os limites do plano. Nesse caso é definido como igual tanto para min,máx dos eixos X e Y.
 plt.axis('equal')
@@ -45,7 +57,7 @@ plt.xlabel("x")
 plt.ylabel("y")
 
 #show = exibe a plotagem dos pontos no plano cartesiano
-# plt.show()
+plt.show()
 
 
 # # Fase 1
@@ -53,7 +65,7 @@ plt.ylabel("y")
 # ## Primeira Abordagem
 # Para cada `cliente (vértice)` calcule o `centro de distribuição` mais próximo.
 
-# In[4]:
+# In[3]:
 
 
 import sys
@@ -61,30 +73,32 @@ import math
 
 colors = ['blue', 'red', 'purple', 'pink', 'orange']
 
+#Fórmula para cálculo de distância Euclidiana entre dois pontos no plano cartesiano
 def euclidian_distance(x1, y1, x2, y2):
     return math.sqrt((x2-x1)**2+(y2-y1)**2)
 
-for i,[point_x,point_y,_,_,_,_] in enumerate(points):
+
+for i,[point_x,point_y,_,_,_,_] in enumerate(clientes):
     menor = sys.maxsize
-    for j,[distribuition_x,distribuition_y,_,_,_] in enumerate(distribuitions):
-        if euclidian_distance(float(point_x), float(point_y), float(distribuition_x), float(distribuition_y)) < menor:
-            menor = euclidian_distance(float(point_x), float(point_y), float(distribuition_x), float(distribuition_y))
-            points[i][5] = j
+    for j,[centros_distribuicao_x,centros_distribuicao_y,_,_,_] in enumerate(centros_distribuicao):
+        if euclidian_distance(float(point_x), float(point_y), float(centros_distribuicao_x), float(centros_distribuicao_y)) < menor:
+            menor = euclidian_distance(float(point_x), float(point_y), float(centros_distribuicao_x), float(centros_distribuicao_y))
+            clientes[i][5] = j
 
 
-for idx,[x,y,_,_,_] in enumerate(distribuitions):
+for idx,[x,y,_,_,_] in enumerate(centros_distribuicao):
     plt.scatter(float(x), float(y), color=colors[idx])
 
-for x,y,_,_,_,dist in points:
+for x,y,_,_,_,dist in clientes:
     plt.scatter(float(x), float(y), color=colors[dist])
     
-for p in points:
+for p in clientes:
     plt.annotate(p[5], xy=(float(p[0]),float(p[1])), xytext=(5,-5), textcoords='offset points')
     
 plt.axis('equal')
 plt.xlabel("x")
 plt.ylabel("y")
-# plt.show()
+plt.show()
 
 
 # ## Segunda Abordagem
@@ -93,7 +107,7 @@ plt.ylabel("y")
 # 
 # Mais informações, leia: https://pt.wikipedia.org/wiki/Diagrama_de_Voronoy
 
-# In[6]:
+# In[4]:
 
 
 from scipy.spatial import Voronoi, voronoi_plot_2d
@@ -101,33 +115,33 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 import numpy as np
 
-vor = Voronoi([(float(x),float(y)) for x,y,_,_,_ in distribuitions])
+vor = Voronoi([(float(x),float(y)) for x,y,_,_,_ in centros_distribuicao])
 voronoi_plot_2d(vor, line_colors='red', show_vertices=False)
 
-for idx,d in enumerate(distribuitions):
+for idx,d in enumerate(centros_distribuicao):
     plt.annotate(vor.point_region[idx], xy=(float(d[0]),float(d[1])), xytext=(0,5), textcoords='offset points')
     
-for x,y,_,_,_,_ in points:
+for x,y,_,_,_,_ in clientes:
     plt.scatter(float(x), float(y), color='blue')
 plt.axis('equal')
 
-# plt.show()
+plt.show()
 
 point = Point(0, 0)
 polygon = Polygon([vor.vertices[i] for i in vor.regions[5] if not i == '-1'])
-print(polygon.contains(point))
+#print(polygon.contains(point))
 
-print("ridge_points\n", vor.ridge_points)
-print("ridge_vertices\n", vor.ridge_vertices)
-print("vertices\n", vor.vertices)
-print("regions\n", vor.regions)
-print("points\n", vor.points)
-print("point_region\n", vor.point_region)
+#print("ridge_points\n", vor.ridge_points)
+#print("ridge_vertices\n", vor.ridge_vertices)
+#print("vertices\n", vor.vertices)
+#print("regions\n", vor.regions)
+#print("points\n", vor.points)
+#print("point_region\n", vor.point_region)
 
 
 # ## Transformação para regiões finitas
 
-# In[332]:
+# In[5]:
 
 
 import numpy as np
@@ -201,7 +215,7 @@ def voronoi_with_finite_polygons(vor):
         new_regions.append(new_region.tolist())
     return new_regions, np.asarray(new_vertices), all_ridges
 
-dists = np.array([[float(x),float(y)] for x,y,_,_,_ in distribuitions])
+dists = np.array([[float(x),float(y)] for x,y,_,_,_ in centros_distribuicao])
 
 # Cria diagrama de Voronoi
 vor = Voronoi(dists)
@@ -237,7 +251,7 @@ for region in regions:
 
 new_points = []
 # Plota os pontos de entrega
-for x,y,_,_,_,_ in points:
+for x,y,_,_,_,_ in clientes:
     plt.scatter(float(x), float(y), color='blue')
     for idx,region in enumerate(new_regions):
         if region.contains(Point(float(x), float(y))):
@@ -254,7 +268,7 @@ plt.axis('equal')
 plt.xlim(vor.min_bound[0] - 20.1, vor.max_bound[0] + 20.1)
 plt.ylim(vor.min_bound[1] - 20.1, vor.max_bound[1] + 40.1)
 
-# plt.show()
+plt.show()
 
 
 # ## Heurística para melhoramento das divisões das regiões
@@ -272,134 +286,51 @@ plt.ylim(vor.min_bound[1] - 20.1, vor.max_bound[1] + 40.1)
 #     2.3 Atualiza as duas regiões (polígonos) com o novo vértice
 # 
 
-# In[335]:
-
-
-# from collections import Counter
-
-# # Estrutura que armazena, em ordem decrescente, a quantidade de pontos por região
-# points_per_region = Counter(np.array(new_points)[:,2]).most_common()
-
-# def region_is_neighborhood(p1,p2):
-#     """ Verifica se duas regiões são vizinhas"""
-#     for points in all_ridges[p1]:
-#         if p2 in points:
-#             return True
-#     return False
-
-# def get_near_to_ridge_between_two_regions(p1,p2):
-#     """ Pegar o ponto mais próximo entre a linha que divide duas regiões desequilibradas"""
-#     pass
-
-# # TODO: Índice não está correto
-# def get_index_in_new_polygon(p1, p2):
-#     """ Retornar o index do novo ponto a ser incluído em um polígono existente """
-#     # Separa os dois vértices que formam a ridge entre os dois polígonos
-#     ridge_vertices = [[v1,v2] for p, v1, v2 in all_ridges[p1] if p == p2][0]
-
-#     for idx, v in enumerate(regions[p1]):
-#         if idx == 0:
-#             pass
-#         else:
-#             if (v, regions[p1][idx-1]) == (ridge_vertices[0], ridge_vertices[1]) or (regions[p1][idx-1], v) == (ridge_vertices[0], ridge_vertices[1]):
-#                 position_p1 = idx - 1
-    
-#     return position_p1
-
-
-# def update_polygon(p1, p2, point):
-#     """
-#     Atualiza polígono(região) com o novo ponto
-    
-#     :param p1: Índice do polígono 1
-#     :param p2: Índice do polígono 2
-#     :param point: Ponto a ser inserido como vértice do polígono. Tupla(x,y).
-#     """
-    
-#     if not region_is_neighborhood(p1,p2):
-#         return False
-    
-#     # TODO: FAZER O MESMO PROCEDIMENTO PARA OS DOIS POLIGONOS, POIS UM IRÁ AUMENTAR ATÉ O PONTO QUISTO E O OUTRO DIMINUIR ATÉ O PONTO QUISTO
-#     # Guarda as coordenadas do poligono a ser alterado
-#     coords_p1 = new_regions[p1].exterior.coords[:]
-#     #coords_p2 = new_regions[p2].exterior.coords[:]
-    
-#     # Posição da região onde será incluído o vértice
-#     position_p1 = get_index_in_new_polygon(p1,p2)
-    
-#     # Inclui o ponto na posição correta
-#     coords_p1 = coords_p1[:position_p1] + [point] + coords_p1[position_p1:]
-#     #coords_p2 = coords[:position_p2] + [point] + coords[position_p2:]
-
-#     # Cria novo poligono e o adiciona como uma região
-#     new_p1 = Polygon(coords_p1)
-#     #new_p2 = Polygon(coords_p2)
-    
-#     new_regions[p1] = new_p1
-#     #new_regions[p2] = new_p2
-    
-# def colorize_polygons():
-#     for poly in new_regions:
-#         polygon = [p for p in poly.exterior.coords]
-#         plt.fill(*zip(*polygon), alpha=0.4)
-
-# update_polygon(0, 4,(10.213724010652633, 30.890770737064063))
-# colorize_polygons()
-# new_points = []
-
-# # Plota os pontos de entrega
-# for x,y,_,_,_,_ in points:
-#     plt.scatter(float(x), float(y), color='blue')
-#     for idx,region in enumerate(new_regions):
-#         if region.contains(Point(float(x), float(y))):
-#             # Para cada ponto de entrega, associa o indice da região
-#             new_points.append([x,y,idx])
-
-
-
-# # Plota os indices das regiões
-# for idx,[x,y] in enumerate(dists):
-#     plt.annotate(idx, xy=(x,y), xytext=(0,-10), textcoords='offset points')
-
-# plt.axis('equal')
-# plt.xlim(vor.min_bound[0] - 20.1, vor.max_bound[0] + 20.1)
-# plt.ylim(vor.min_bound[1] - 20.1, vor.max_bound[1] + 40.1)
-
-# plt.show()
-
-
-# # Fase 2
+# # Resultado da Fase 1
 # 
-# ...
+# ## Clientes
+
+# In[6]:
+
+
 from cliente import Cliente
-from centro import Centro
 
-# Instancia os clientes
-clientes = []
+lista_de_clientes = []
 
-for idx, point in enumerate(points):
-    
-    coordenadas = []
-    coordenadas.append(point[0])
-    coordenadas.append(point[1])
+for idx, point in enumerate(clientes):
+    coordernadas = []
+    coordernadas.append(point[0])
+    coordernadas.append(point[1])
     volume = float(point[2])
     preco_mercadoria = float(point[3])
     qtd_pacotes = float(point[4])
-    centro = point[5]
-    qtd_vizinhos = len(points)
+    regiao = point[5]
+    qtd_vizinhos = len(clientes)
+    #Criada lista de Objetos do Tipo CLIENTE
+    lista_de_clientes.append(Cliente(coordernadas, volume, preco_mercadoria, qtd_pacotes, regiao, idx, qtd_vizinhos))
 
-    clientes.append(Cliente(coordenadas, volume, preco_mercadoria, qtd_pacotes, centro, idx, qtd_vizinhos))
+[print(cliente) for cliente in lista_de_clientes]
+
+
+# ## Centros
+
+# In[7]:
+
+
+from centro import Centro
 
 #Instancia os centros de distribuicao
-centros = []
+lista_de_centros = []
 
-for idx, center in enumerate(distribuitions):
+for idx, center in enumerate(centros_distribuicao):
     coordenadas = []
     coordenadas.append(center[0])
     coordenadas.append(center[1])
     clientes_do_centro = []
     #  Pega todos os clientes que pertencem ao centro em questao
-    clientes_do_centro = [cliente for cliente in clientes if cliente.get_centro() == idx]
+    clientes_do_centro = [cliente for cliente in lista_de_clientes if cliente.get_centro() == idx]
     label = idx
 
-    centros.append(Centro(coordenadas, clientes_do_centro, label))
+    lista_de_centros.append(Centro(coordenadas, clientes_do_centro, label))
+    
+[print(centro) for centro in lista_de_centros]
